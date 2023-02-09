8385184eb8e18152b9a008d07d8097de5616df4f
lorenzo-osimani <lorenzo.osimani@studio.unibo.it>
2023-02-09 18:34:14 +0100

    Defining starting classes

--- solve-lpaas/src/main/kotlin/it/unibo/tuprolog/solve/lpaas/Placeholder.kt
+++ solve-lpaas/src/main/kotlin/it/unibo/tuprolog/solve/lpaas/Placeholder.kt
@@ -1,14 +1,44 @@
 package it.unibo.tuprolog.solve.lpaas
 
+import io.grpc.ManagedChannelBuilder
 import io.grpc.ServerBuilder
+import io.grpc.stub.StreamObserver
+import java.util.concurrent.TimeUnit
 
-
-// TODO this file is just a placeholder, and it can be removed
-
-fun main(args: Array<String>) {
+fun main(/*args: Array<String>*/) {
     val server = ServerBuilder.forPort(8080)
-        .addService(MyGreetingService)
+        .addService(SolverService)
+        .build()
+    server.start()
+
+    val prova: SolveRequest = SolveRequest.newBuilder().setStruct("bla").build()
+
+    val replyPrinter: StreamObserver<SolutionReply> = object : StreamObserver<SolutionReply> {
+        override fun onNext(value: SolutionReply) {
+            println(value.solution)
+        }
+
+        override fun onError(t: Throwable) {
+            t.printStackTrace()
+        }
+
+        override fun onCompleted() {
+            // do nothing
+        }
+    }
+
+    val channel = ManagedChannelBuilder.forAddress("localhost", 8080)
+        .usePlaintext()
         .build()
 
-    server.start()
+    val client = SolverGrpc.newStub(channel)
+
+    client.solve(prova, replyPrinter)
+
+    channel.shutdown()
+    channel.awaitTermination(1, TimeUnit.SECONDS)
+
+    println("Listening on port " + 8080)
+    server.awaitTermination()
+    Runtime.getRuntime().addShutdownHook(Thread { server.shutdownNow() })
 }
