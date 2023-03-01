633aeac9a177a09b8eb81494fb4db756289e9f1c
lorenzo-osimani <lorenzo.osimani@studio.unibo.it>
2023-03-01 10:31:54 +0100

    Saving before Branching

--- solve-lpaas/src/main/kotlin/it/unibo/tuprolog/solve/lpaas/client/prolog/SolutionSequence.kt
+++ solve-lpaas/src/main/kotlin/it/unibo/tuprolog/solve/lpaas/client/prolog/SolutionSequence.kt
@@ -2,6 +2,7 @@ package it.unibo.tuprolog.solve.lpaas.client.prolog
 
 import com.google.common.util.concurrent.Futures
 import io.grpc.ManagedChannelBuilder
+import io.grpc.StatusRuntimeException
 import it.unibo.tuprolog.core.*
 import it.unibo.tuprolog.core.exception.TuPrologException
 import it.unibo.tuprolog.core.parsing.parse
@@ -14,6 +15,7 @@ import it.unibo.tuprolog.solve.lpaas.SolutionReply
 import it.unibo.tuprolog.solve.lpaas.SolverGrpc
 import kotlinx.coroutines.*
 import kotlinx.coroutines.Dispatchers.Unconfined
+import java.util.concurrent.ExecutionException
 import java.util.concurrent.Executor
 import java.util.concurrent.Future
 
@@ -40,11 +42,11 @@ class SolutionSequence(val solverId: String, val query: String): Sequence<Soluti
             }
 
             fun getNextResult(): Solution {
+                val struct = Struct.parse(query)
                 val reply = clientGetSolution.nextSolution(
                     NextSolutionRequest.newBuilder()
                         .setQuery(query).setId(solverId).build()
                 ).get()
-                val struct = Struct.parse(query)
                 val scope = Scope.of(struct.args.filter { it.isVar }.map { it.castToVar() })
                 println(struct)
                 return if (reply.isYes) {
@@ -52,13 +54,14 @@ class SolutionSequence(val solverId: String, val query: String): Sequence<Soluti
                     reply.substitutionList.forEach { pair ->
                         unifiers[scope.varOf(pair.`var`)] = Term.parse(pair.term)
                     }
-                    println(unifiers.keys + unifiers.values)
                     Solution.yes(struct, Substitution.unifier(unifiers))
                 } else {
                     nextFlag = false
-                    return if (reply.isNo) Solution.no(struct) else return Solution.no(struct)
+                    println(reply.error)
+                    return if (reply.isNo) Solution.no(struct)
                     /** Fix Error **/
-                    //else Solution.halt(struct, ResolutionException(TuPrologException("")))
+                    else Solution.no(struct)
+
                 }
             }
         }
