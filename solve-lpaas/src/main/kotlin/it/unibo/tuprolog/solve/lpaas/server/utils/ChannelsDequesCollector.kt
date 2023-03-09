c22f31eca466237a3449abc20976f4026d3fd0af
lorenzo-osimani <lorenzo.osimani@studio.unibo.it>
2023-03-09 17:08:45 +0100

    Implemeted MutableSolver

--- solve-lpaas/src/main/kotlin/it/unibo/tuprolog/solve/lpaas/server/utils/ChannelsDequesCollector.kt
+++ solve-lpaas/src/main/kotlin/it/unibo/tuprolog/solve/lpaas/server/utils/ChannelsDequesCollector.kt
@@ -1,34 +1,55 @@
 package it.unibo.tuprolog.solve.lpaas.server.utils
 
+import it.unibo.tuprolog.solve.channel.InputChannel
+import it.unibo.tuprolog.solve.channel.OutputChannel
 import it.unibo.tuprolog.solve.lpaas.util.toMap
 import java.util.concurrent.BlockingDeque
 import java.util.concurrent.LinkedBlockingDeque
 
-class ChannelsDequesCollector(inputs: Set<String> = emptySet(),
-                              outputs: Set<String> = emptySet(),) {
-    private val inputs: MutableMap<String, BlockingDeque<String>> = mutableMapOf()
-    private val outputs: MutableMap<String, BlockingDeque<String>> = mutableMapOf()
+class ChannelsDequesCollector() {
+    private val inputs: MutableMap<String, Pair<BlockingDeque<String>, InputChannel<String>>> = mutableMapOf()
+    private val outputs: MutableMap<String, Pair<BlockingDeque<String>, OutputChannel<String>>> = mutableMapOf()
 
-    init {
-        inputs.forEach { this.inputs[it] = LinkedBlockingDeque() }
-        outputs.forEach { this.outputs[it] = LinkedBlockingDeque() }
+    fun addInputChannel(name: String, content: String = ""): InputChannel<String> {
+        val deque = LinkedBlockingDeque<String>()
+        val channel = InputChannel.of { deque.takeFirst() }
+        inputs[name] = Pair(deque, channel)
+        if(content.isNotEmpty()) writeOnInputChannel(name, content)
+        return channel
     }
 
-    fun getAllInputs(): Map<String, BlockingDeque<String>> {
-        return inputs
+    fun addOutputChannel(name: String): OutputChannel<String> {
+        val deque = LinkedBlockingDeque<String>()
+        val channel = OutputChannel.of { line: String-> deque.putLast(line) }
+        outputs[name] = Pair(deque, channel)
+        return channel
     }
 
-    fun getAllOutputs(): Map<String, BlockingDeque<String>> {
-        return outputs.map { Pair(it.key, it.value) }.toMap()
+    fun getInputChannels(): Map<String, InputChannel<String>> {
+        return inputs.map { Pair(it.key, it.value.second) }.toMap()
+    }
+
+    fun getOutputChannels(): Map<String, OutputChannel<String>> {
+        return outputs.map { Pair(it.key, it.value.second) }.toMap()
     }
 
     fun writeOnInputChannel(name: String, line: String = "") {
-        line.toCharArray().forEach { inputs[name]?.putLast(it.toString()) }
+        line.toCharArray().forEach { inputs[name]?.first!!.putLast(it.toString()) }
     }
 
     fun readOnOutputChannel(name: String): String {
         if(outputs.containsKey(name))
-           return outputs[name]!!.takeFirst()
+           return outputs[name]!!.first.takeFirst()
         else throw IllegalArgumentException()
+    }
+
+    companion object {
+        fun of(inputs: Map<String, String> = emptyMap(),
+               outputs: Set<String> = emptySet()): ChannelsDequesCollector {
+            val collection = ChannelsDequesCollector()
+            inputs.forEach { collection.addInputChannel(it.key, it.value) }
+            outputs.forEach { collection.addOutputChannel(it) }
+            return collection
+        }
     }
 }
